d4fd0031c1ef3ae1f8d54b5091a8356f3ec7512d
Jensemandse <jens.thyregod@gmail.com>
2023-09-07 20:13:54 +0200

    added stopword removal for bengali

--- week_36/Preprocessor.py
+++ week_36/Preprocessor.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import arabicstopwords.arabicstopwords as stp
+from bltk.langtools import remove_stopwords, Tokenizer
 
 class DataFramePreprocessor:
     
@@ -8,6 +9,7 @@ class DataFramePreprocessor:
         self._add_tokens_column()
         self._clean_tokens()
         self._remove_arabic_stopwords()
+        self._remove_bengali_stopwords()
 
     def _add_tokens_column(self):
         self.df['tokens'] = self.df['document_plaintext'].str.split()
@@ -21,6 +23,18 @@ class DataFramePreprocessor:
         self.df[column_name] = self.df[column_name].apply(lambda tokens: [clean_token(token) for token in tokens])
 
     def _remove_arabic_stopwords(self, column_name='tokens'):
-        self.df[column_name] = self.df[column_name].apply(lambda tokens: [token for token in tokens if not stp.is_stop(token)])
+        def filter_arabic_stopwords(tokens, lang):
+            if lang == "Arabic":
+                return [token for token in tokens if not stp.is_stop(token)]
+            return tokens
+        
+        self.df[column_name] = self.df.apply(lambda row: filter_arabic_stopwords(row[column_name], row['language']), axis=1)
 
+    def _remove_bengali_stopwords(self, column_name='tokens'):
+        def filter_bengali_stopwords(tokens, lang):
+            if lang == "Bengali":
+                return remove_stopwords(tokens, level='moderate')  # You can change the level as needed
+            return tokens
+        
+        self.df[column_name] = self.df.apply(lambda row: filter_bengali_stopwords(row[column_name], row['language']), axis=1)
 
